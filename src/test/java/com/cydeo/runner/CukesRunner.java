6a0e96f9b73ad0914c8a4747ea32fec464c49687
vildanzora <orgunvildan@gmail.com>
2022-07-14 12:06:24 -0500

    update

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -7,18 +7,17 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        plugin = { "json:target/json-report/cucumber.json",
-                "html:target/cucumber-report.html",
-                "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
-                "junit:target/MyReports-report.xml"
-        },
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt",},
 
         features = "src/test/resources/features" ,
         glue = "com/cydeo/step_definitions",
         dryRun = false,
         //true = for get snippets
-        tags = "@userstory4"  // it will run all @smoke tags scenarios
+        tags = "@smoke"  // it will run all @smoke tags scenarios
         //I will determine common tag for all of my future. @Regression and not @student
 )
 public class CukesRunner {
