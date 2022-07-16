355709d9b6340d103b1309af1dabe73c21824b23
vildanzora <orgunvildan@gmail.com>
2022-07-16 11:49:28 -0500

    update

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         glue = "com/cydeo/step_definitions",
         dryRun = false,
         //true = for get snippets
-        tags = "@smoke"  // it will run all @smoke tags scenarios
+        tags = ""  // it will run all @smoke tags scenarios
         //I will determine common tag for all of my future. @Regression and not @student
 )
 public class CukesRunner {
